2fe8de32931d45e20923b4e92cf7040cd92ff5ed
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2025-01-08 15:40:27 +0100

    depotdownloader.sh: fix comments and add don't print success to -V or --version

--- depotdownloader.sh
+++ depotdownloader.sh
@@ -1,19 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/env sh
-#WIP
-#setting env
+# WIP
+# setting env var
 username="user"
+# end of setting env var
+# setting function
 print_green() {
-	#Bold bright green
+	# Bold bright green
 	printf "\033[1;92m%s\033[0m\n" "$1"
 }
 print_red() {
-	#Bold red
+	# Bold red
 	printf "\033[1;31m%s\033[0m\n" "$1"
 }
-#end of setting env
+# end of setting function
 proot-distro login alpine --user $username --shared-tmp -- ash -c "cd /storage/emulated/0/Download/depotdownloaded && DepotDownloader \"\$@\"" -- "$@"
 if [ $? -eq 0 ]; then
-	if [ "$#" -ne 0 ]; then
+	if [ "$#" -ne 0 ] && ! echo "$@" | grep -qE '(^|\s)(-V|--version)($|\s)'; then
 		print_green "Download finished! Check the 'Downloads/depotdownloaded' folder in your file manager app."
 	fi
 else
