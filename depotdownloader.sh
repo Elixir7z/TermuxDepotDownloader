be3b1c92a0f016558e9f3c4fadf5b00c89d47a05
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2025-02-18 15:57:58 +0100

    depotdownloader.sh: Added `--no-arch-warning`, to suppress unnecessary warning about 32bit support.
    This suppress unnecessary warning message about 32bit support for any SoC without 32bit support since we used software for correct arch only.

--- depotdownloader.sh
+++ depotdownloader.sh
@@ -13,7 +13,7 @@ print_red() {
 	printf "\033[1;31m%s\033[0m\n" "$1"
 }
 # end of setting function
-proot-distro login alpine --user $username --shared-tmp -- ash -c "cd /storage/emulated/0/Download/depotdownloaded && DepotDownloader \"\$@\"" -- "$@"
+proot-distro login alpine --user $username --no-arch-warning --shared-tmp -- ash -c "cd /storage/emulated/0/Download/depotdownloaded && DepotDownloader \"\$@\"" -- "$@"
 if [ $? -eq 0 ]; then
 	if [ "$#" -ne 0 ] && ! echo "$@" | grep -qE '(^|\s)(-V|--version)($|\s)'; then
 		print_green "Download finished! Check the 'Downloads/depotdownloaded' folder in your file manager app."
